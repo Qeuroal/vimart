ec0e3a88ccf4a2e11bc6c338d9a915c02dab348d
Qeuroal <12997837+qeuroal@user.noreply.gitee.com>
2024-04-20 18:03:27 +0800

    local install vimart

--- scripts/install_function_on_linux.sh
+++ scripts/install_function_on_linux.sh
@@ -108,19 +108,19 @@ function local_install_vimart_on_linux() {
     referenceUser=${VIMART_REFERENCE_USER}
     if [ "${referenceUser}" != "" ]; then
         color_print "warning" "reference user: ${referenceUser}"
-        if [ -d ~${referenceUser}/.vim ]; then
+        if [[ -d ~${referenceUser}/.vim ]]; then
             sudo cp -rf ~${referenceUser}/.vim ${destPath}
         else
             color_print "warning" "don't exist ~${referenceUser}/.vim"
         fi
 
-        if [ -f ~${referenceUser}/.vimrc.custom.config ]; then
+        if [[ -f ~${referenceUser}/.vimrc.custom.config ]]; then
             cp -f ~${referenceUser}/.vimrc.custom.config ${destPath}
         else
             color_print "warning" "don't exist ~${referenceUser}/.vimrc.custom.config"
         fi
 
-        if [ -f ~${referenceUser}/.vimrc.custom.plugins ]; then
+        if [[ -f ~${referenceUser}/.vimrc.custom.plugins ]]; then
             cp -f ~${referenceUser}/.vimrc.custom.plugins ${destPath}
         else
             color_print "warning" "don't exist ~${referenceUser}/.vimrc.custom.plugins"
